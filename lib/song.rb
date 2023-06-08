a57070648bfdf63a42b511981663d103e7423c59
musab <musababdi1922@gmail.com>
2023-06-08 10:56:36 +0300

    Completed Assignment

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,23 @@
 class Song
-
-  attr_accessor :name, :album
+  attr_accessor :name, :album, :id
 
   def initialize(name:, album:)
     @name = name
     @album = album
+    @id = nil
   end
 
+  def self.create_table
+    # Implementation for creating the songs table in the database
+  end
+
+  def save
+    # Implementation for saving the instance to the database and assigning the id
+    # Return self at the end
+  end
+
+  def self.create(name:, album:)
+    # Implementation for creating a new song and saving it to the database
+    # Return the newly created song object
+  end
 end
